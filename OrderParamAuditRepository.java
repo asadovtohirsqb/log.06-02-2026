b646b7966ff9dc8a61b941400521eafdc8dac25a
asadovtohirsqb <asadovtohirsqb@gmail.com>
2026-03-19 16:30:10 +0500

    Delete OrderParamAuditRepository.java

--- OrderParamAuditRepository.java
+++
@@ -1,7 +0,0 @@
-package uz.sqb.joyda.carddeliveryservice.repository.card_delivery;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uz.sqb.joyda.carddeliveryservice.domain.card_delivery.OrderParamAudit;
-
-public interface OrderParamAuditRepository extends JpaRepository<OrderParamAudit, Long> {
-}
