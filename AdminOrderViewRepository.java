acff965638d7283ddff9edd58be6cd7fa74e08ef
asadovtohirsqb <asadovtohirsqb@gmail.com>
2026-03-19 16:29:15 +0500

    Delete AdminOrderViewRepository.java

--- AdminOrderViewRepository.java
+++
@@ -1,8 +0,0 @@
-package uz.sqb.joyda.carddeliveryservice.repository.card_delivery;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import uz.sqb.joyda.carddeliveryservice.domain.card_delivery.AdminOrderView;
-
-public interface AdminOrderViewRepository extends JpaRepository<AdminOrderView, Long>, JpaSpecificationExecutor<AdminOrderView> {
-}
